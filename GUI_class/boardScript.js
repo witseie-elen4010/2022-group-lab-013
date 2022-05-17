7a02727935dac87f92510ffce6e09b6c3583bacf
Keowa-1877616 <1877616@students.wits.ac.za>
2022-05-17 23:22:18 +0200

    Added comments and cleaned up code

--- GUI_class/boardScript.js
+++ GUI_class/boardScript.js
@@ -1,55 +1,53 @@
 /* eslint-env jest */
-//import { WORDS } from './words.js'
-const noOfGuesses = 6
 
 //Function to initializa game board
-function initBoard () {
-  let board = document.getElementById('game-board') //board is of type HTMLElement
+function GameBoardInitialization () {
+  let gameBoard = document.getElementById('game-board') //gameBoard is of type HTMLElement
 
-  for (let i = 0; i < 6; i++) { //Create 6 rows
-    let row = document.createElement('div') //row is of type HTMLElement
-    row.className = 'letter-row'
+  for (let i = 0; i < 6; i++) { //Create 6 word containers
+    let wordContainer = document.createElement('div') //wordContainer is of type HTMLElement
+    wordContainer.className = 'letter-row'
 
-    for (let j = 0; j < 5; j++) { //Create 5 boxes for each row
-      let box = document.createElement('div') //box is of type HTMLElement
-      box.className = 'letter-box'
-      row.appendChild(box)  //Add box as a child element of the current row
+    for (let j = 0; j < 5; j++) { //Create 5 letter container for each wordContainer
+      let letterContainer = document.createElement('div') //letterContainer is of type HTMLElement
+      letterContainer.className = 'letter-box'
+      wordContainer.appendChild(letterContainer)  //Add letterContainer as a child element of the current wordContainer
     }
-    board.appendChild(row)  //Add row as a child element of the board
+    gameBoard.appendChild(wordContainer)  //Add wordContainer as a child element of the board
   }
   console.log('Board Initialised')  //Log message used for testing
 }
 
-//Function to populate a given row with a given word
-function populateRow (guessesWord, rowNo) {
-  let row = document.getElementsByClassName('letter-row')[rowNo]  //row is specified by rowNo
+//Function to populate a given wordContainer with a given word
+function UpdateWordContainer (guessesWord, guessNo) {
+  let wordContainer  = document.getElementsByClassName('letter-row')[guessNo]  //wordContainer is specified by guessNo
 
-  for (let i = 0; i < 5; i++) { //Insert each letter of the given word (guessesWord) into boxes of row
-    let box = row.children[i]
-    box.textContent = guessesWord[i]
-    box.classList.add('filled-box')
+  for (let i = 0; i < 5; i++) { //Insert each letter of the given word (guessesWord) into letterContainers of wordContainer
+    let letterContainer = wordContainer.children[i]
+    letterContainer.textContent = guessesWord[i]
+    letterContainer.classList.add('filled-box')
   }
 }
 
-//Function to change the colour of a given box in a given row
-function changeBoxColour (boxCorrectnessStatus, rowNo, boxNo) {
-  let row = document.getElementsByClassName('letter-row')[rowNo]  //row is specified by rowNo
-  let box = row.children[boxNo] //box is specified by boxNo
+//Function to change the colour of a given box in a given wordContainer
+function ChangeLetterContainerColour (boxCorrectnessStatus, guessNo, letterNo) {
+  let wordContainer = document.getElementsByClassName('letter-row')[guessNo]  //wordContainer is specified by guessNo
+  let letterContainer = wordContainer.children[letterNo] //letterContainer is specified by letterNo
 
   if (boxCorrectnessStatus === 'wrong') {
-    box.style.backgroundColor = 'grey'  //Grey means letter is not in word
+    letterContainer.style.backgroundColor = 'grey'  //Grey means letter is not in word
   } else
   if (boxCorrectnessStatus === 'wrongPos') {
-    box.style.backgroundColor = '#baa817' //Yellow means letter is in word but in the wrong position
+    letterContainer.style.backgroundColor = '#baa817' //Yellow means letter is in word but in the wrong position
   } else
   if (boxCorrectnessStatus === 'correct') {
-    box.style.backgroundColor = 'green' //Green means the letter is in word and correct position
+    letterContainer.style.backgroundColor = 'green' //Green means the letter is in word and correct position
   }
 }
 
 //Initialize the board
-initBoard()
+GameBoardInitialization()
 //Export functions for use in other .js files
-export {initBoard}
-export {populateRow}
-export {changeBoxColour}
+export {GameBoardInitialization as initBoard}
+export {UpdateWordContainer  as populateRow}
+export {ChangeLetterContainerColour as changeBoxColour}
