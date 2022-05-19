244591766c28a24db73c473926fecfa0dd08ce5c
FalseCrystal <kakitleong8@gmail.com>
2022-05-19 16:01:38 +0200

    helloworld test

--- gameRoutes.js
+++ gameRoutes.js
@@ -1,6 +1,9 @@
 const path = require('path')
 const express = require('express')
 const router = express.Router()
+router.get('/',function(req,res){
+  res.send('Hello World')
+})
 router.get('/SinglePlayer', function (req, res) {
 res.sendFile(path.join(__dirname,'Public', 'View', 'SinglePlayer.html'))
 })
