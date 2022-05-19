504d48b051fb2618baae16f7f3ab0f5233915bca
FalseCrystal <kakitleong8@gmail.com>
2022-05-19 14:14:50 +0200

    Removed test line, now testing azure hosting program

--- gameRoutes.js
+++ gameRoutes.js
@@ -1,9 +1,6 @@
 const path = require('path')
 const express = require('express')
 const router = express.Router()
-router.get('/',function(req,res){
-  res.send('Hello World')
-})
 router.get('/SinglePlayer', function (req, res) {
 res.sendFile(path.join(__dirname,'Public', 'View', 'SinglePlayer.html'))
 })
