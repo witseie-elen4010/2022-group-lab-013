274d39a8e2c25a8f2f5fb669203d22df1b5747e9
FalseCrystal <kakitleong8@gmail.com>
2022-05-19 15:43:43 +0200

    Changed public file directory assignment to Folder "Public", changes file paths in html to match

--- index.js
+++ index.js
@@ -5,10 +5,9 @@ const app = express()
 const gameRouter = require('./gameRoutes.js')
 // mounting our routers
 
+
 app.use('/', gameRouter)
-app.use('/cdn', express.static('Public'));
-
-
+app.use('/Public', express.static('Public'));
 const port = process.env.PORT || 3000
 app.listen(port)
 console.log('Express server running on port', port)
