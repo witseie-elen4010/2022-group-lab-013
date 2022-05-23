3c36f6411554b6b6c8cb670a2546d5e4ac398de3
Hyla Fourie <1835707@students.wits.ac.za>
2022-05-23 11:02:45 +0200

    Variable naming fixed and debug statements removed

--- Public/Script/CheckLetterInWord.js
+++ Public/Script/CheckLetterInWord.js
@@ -1,38 +1,35 @@
 'use strict'
 
-function CheckLetterInWord(wordToCheck,templateWord){
-    let arrayLength = wordToCheck.length;
+function CheckLetterInWord(userInputWord,randomWordToGuess){
+    let arrayLength = userInputWord.length;
     let rightnessArray =[];
-    let repeatedTemplate = false;
+    let isRepeatedInWordToGuess = false;
     let repeatLetter='';
     let firstRepeat = -1;
     for (let i =0;i<arrayLength;i++){
-        let posIfInWord = templateWord.indexOf(wordToCheck[i]);
+        let posIfInWord = randomWordToGuess.indexOf(userInputWord[i]);
         ;
         
-        if(posIfInWord != -1 && templateWord.indexOf(wordToCheck[i],posIfInWord+1)!=-1){
-            repeatedTemplate = true;
-            repeatLetter=wordToCheck[i];
+        if(posIfInWord != -1 && randomWordToGuess.indexOf(userInputWord[i],posIfInWord+1)!=-1){
+            isRepeatedInWordToGuess = true;
+            repeatLetter=userInputWord[i];
         }
 
-        if(wordToCheck.indexOf(wordToCheck[i],i+1)!=-1&&firstRepeat==-1)
-        {
+        if(userInputWord.indexOf(userInputWord[i],i+1)!=-1&&firstRepeat==-1){
             firstRepeat=i;
-            console.log(firstRepeat)
         }
+        //**************Actual checks********************//
         if(posIfInWord === -1){
             rightnessArray[i] = 'wrong';//returns 'wrong' if letter is wrong
         }
-        else if (wordToCheck[i] === templateWord[i]){
+        else if (userInputWord[i] === randomWordToGuess[i]){
             rightnessArray[i] = 'correct';//returns 'correct' if letter is in correct place
         }
         else{
             rightnessArray[i] = 'wrongPos';//returns 'wrongPos' if letter is in word but in the wrong place
-            console.log('loop')
-            console.log(i)
-            if(!repeatedTemplate && firstRepeat!=-1 && i!=firstRepeat){
+            if(!isRepeatedInWordToGuess && firstRepeat!=-1 && i!=firstRepeat){
                 rightnessArray[i] = 'wrong';
-                console.log('here')
+                //This code accounts for repeated letters in the input wourd
             }
         }
         
