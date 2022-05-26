a258dc6821ffe756a1c2e93c3fcb6f67a47980df
Keowa-1877616 <1877616@students.wits.ac.za>
2022-05-26 18:10:21 +0200

    Changed startup page and fixed login page bug

--- gameRoutes.js
+++ gameRoutes.js
@@ -3,7 +3,8 @@ const express = require('express')
 const gameRouter = express.Router()
 
 gameRouter.get('/', function (req, res) {
-res.sendFile(path.join(__dirname,'Public', 'View', 'SinglePlayer.html'))
+//res.sendFile(path.join(__dirname,'Public', 'View', 'SinglePlayer.html'))
+res.sendFile(path.join(__dirname,'Public', 'View', 'loginPage.html'))
 })
 
 module.exports = gameRouter
--- index.js
+++ index.js
@@ -15,6 +15,8 @@ console.log('Express server running on port', port)
 //http://127.0.0.1:3000/SinglePlayer for checkingno
 
 //Test MongoDb
+/*
 const functions = require('./Private/Script/DatabaseHandler');
 const ConnectToDatabase = functions.ConnectToDatabase;
 ConnectToDatabase();
+*/
