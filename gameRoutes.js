992a95e6768ff3349134b4311beb456582ed71d6
Tshireletso Rasemana <1981901@students.wits.ac.za>
2022-05-26 23:08:20 +0200

    Added login to Home Page nevigation

--- gameRoutes.js
+++ gameRoutes.js
@@ -3,6 +3,10 @@ const express = require('express')
 const gameRouter = express.Router()
 
 gameRouter.get('/', function (req, res) {
+res.sendFile(path.join(__dirname,'Public', 'View', 'loginPage.html'))
+})
+
+gameRouter.get('/homePage', function (req, res) {
 res.sendFile(path.join(__dirname,'Public', 'View', 'homePage.html'))
 })
 
