9713f9d3d29cbdced06e450c3f7fca4689a14165
Hyla Fourie <1835707@students.wits.ac.za>
2022-05-26 18:48:00 +0200

    issue with require "fixed"

--- Public/Script/login.js
+++ Public/Script/login.js
@@ -1,21 +1,26 @@
-const functions = require('../../Private/Script/DatabaseHandler');
+const functions = require('../../Private/Script/DatabaseHandler.js');
 const ConnectToDatabase = functions.ConnectToDatabase;
 const ValidLogin = functions.ValidLogin;
 
 let button = document.getElementById("loginButton");
+console.log('here')
 
 button.addEventListener('click', function () {
   ConnectToDatabase();
 
   var username = document.getElementById("playerUsername");
+  console.log(username);
   var password = document.getElementById("playerPassword");
+  console.log(password);
 
   ValidLogin(username, password).then(response => {
     let passed = response;
     if (passed) {
-      alert("Loading");
+      console.log("Loading");
+      window.location.href='../View/SinglePlayer.html';
     } else {
       alert("Incorrect username or password");
+      console.log("wrong");
     }
   });
 
