844183dcbc3d0fb06d124ff89f8d887658850394
FalseCrystal <kakitleong8@gmail.com>
2022-05-26 14:13:00 +0200

    Edited end game pop up to have a button to restart the game with a new word. edited scripts for board reset and game play control

---
+++ Public/Script/EndGameManager.js
@@ -0,0 +1,71 @@
+'use strict'
+
+let turns = 0
+let winFlag = false
+let endFlag = false
+let gameOverText =''
+
+function OpenForm(endGamePopUp){
+    //console.log('opening')
+    endGamePopUp.style.display ='block'
+}
+
+function CloseForm(endGamePopUp){
+    //console.log('closing')
+   endGamePopUp.style.display = 'none'
+}
+
+function Restart(endGamePopUp){
+    turns = 0
+    winFlag = false
+    endFlag = false
+    gameOverText =''
+    endGamePopUp.style.display = 'none'
+}
+function IncreaseTurnCounter(){
+    turns+=1
+    return turns
+}
+
+function EndGameCheck(rightnessArray,answer,endGamePopUp,popUpText){
+    if(!endFlag){
+        for(let i = 0;i<rightnessArray.length;i++){
+        if(rightnessArray[i]!='correct'){
+            winFlag = false
+            break}
+
+        else if(rightnessArray[i]=='correct'){
+            winFlag = true
+        }
+    }
+
+    if(winFlag === true){
+        gameOverText = `You have won in ${turns} tries\n The correct word is \n ${answer.toUpperCase()}`
+            //setTimeout(function(){alert(gameOverText)
+            
+        endFlag = true
+        console.log("win",endFlag)
+            }
+        
+    if (turns === 6 && winFlag === false){
+        gameOverText = `Game Over \n The correct word is \n ${answer.toUpperCase()}`
+            //setTimeout(function(){alert(gameOverText)
+            
+            endFlag = true
+            console.log("gameover",endFlag)
+        }
+    
+    }
+
+    if(endFlag === true){
+        popUpText.textContent = gameOverText
+        OpenForm(endGamePopUp)
+        //gameOverText = `You have given up in ${turns} tries \n The correct word is \n ${answer.toUpperCase()}`
+    }
+    
+    
+    return endFlag
+}
+
+export {EndGameCheck,IncreaseTurnCounter,CloseForm,Restart}
+
