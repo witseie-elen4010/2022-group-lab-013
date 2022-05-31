64fe3c94dad5fa406675321fb2bce5b239894866
Hyla Fourie <1835707@students.wits.ac.za>
2022-05-31 18:42:07 +0200

    Navigation between homepage and login dynamically fixed

--- Public/Script/login.js
+++ Public/Script/login.js
@@ -1,13 +1,22 @@
 
 let button = document.getElementById("loginButton")
 
-button.addEventListener('click', function () {
+function validation() {
+
   var username = document.getElementById("playerUsername")
   var password = document.getElementById("playerPassword")
+
   if (username.value == "Guest" && password.value == "Guest") {
-        window.location.href ="homePage";
-      } else {
-        window.location.href ="loginPage";
-        alert('Wrong username or password');
-      }
-})
+
+    document.getElementById("form_id").action = "homePage"
+    document.getElementById("form_id").submit();
+  } else {
+    alert('Wrong username or password');
+  }
+    
+  if (username.value === '' || password.value === '') {
+    alert('Please fill in all fields');
+  }
+
+
+}
