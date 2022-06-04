a40d149e4f9bb30c104b2db37c2ddce0f34916c0
Cynthia Kijjambu <ckijjambu.tns@hotmail.com>
2022-06-04 17:01:59 +0200

    user can see opponents board - fixed pull request changes

--- Public/Script/multiplayer.js
+++ Public/Script/multiplayer.js
@@ -1,7 +1,10 @@
 
-function ToMultiPlayerGamePage() {
-  document.getElementById("multiPlayer_id").action = "multiplayerGame"
+function ToMultiPlayerSingleWordPage() {
+  document.getElementById("multiPlayer_id").action = "multiplayerSingleWord"          //navigate to multiplayerSingleWord when button is clicked
   document.getElementById("multiPlayer_id").submit();
 }
 
-
+function ToMultiPlayerGamePage() {
+  document.getElementById("multiPlayer_id").action = "multiplayerGame"                //navigate to multiplayerSingleWord when button is clicked
+  document.getElementById("multiPlayer_id").submit();
+}
